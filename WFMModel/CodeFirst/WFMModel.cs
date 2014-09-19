e3699db159d96ec166d899c3e608bc6882943d32
Muha-a <AnMukha@yandex.ru>
2014-09-19 13:22:13 +0300

    Add missed files

---
+++ WFMModel/CodeFirst/WFMModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+
+namespace WFMModel
+{
+    public class WFMModelContainer : DbContext
+    {
+        public WFMModelContainer()
+            : base("WFMModelContainer")
+        {
+        }
+
+        public DbSet<TagTrigger> TagTriggerSet { get; set; }
+        public DbSet<GPUTypicalCause> GPUTypicalCauseSet { get; set; }
+        public DbSet<Employee> EmployeeSet { get; set; }
+        public DbSet<GPUCause> GPUCauseSet { get; set; }
+        public DbSet<GPUStop> GPUStopSet { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
